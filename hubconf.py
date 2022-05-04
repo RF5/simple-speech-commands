a9acd7c15256a923414683d1e32aa3588fa5946c
RF5 <RF5@users.noreply.github.com>
2022-05-04 14:53:56 +0200

    add resnext model

--- hubconf.py
+++ hubconf.py
@@ -1,7 +1,12 @@
 dependencies = ['torch', 'torchaudio', 'numpy', 'omegaconf']
 
+from dis import pretty_flags
+from msilib.schema import ProgId
+from random import paretovariate
 from convrnn_classifier import ConvRNNClassifier, ConvRNNConfig
 from train import TrainConfig, DistributedConfig
+from sc09_resnext import ResNextWrapper
+from sc09_resnext import CLASSES as RESNEXT_CLASSES
 import torch
 from omegaconf import OmegaConf
 
@@ -62,3 +67,28 @@ def convgru_classifier_sc09(pretrained=True, progress=True, device='cuda', type=
     model.classes = classes_sc09
     model.eval()
     return model
+
+
+def resnext_classifier_sc09(pretrained=True, progress=True, device='cuda'):
+    f"""
+    ResNeXT classifier model trained Google Speech Commands digits dataset (SC09).
+    Model takes in 16kHz raw waveform with samples in the range of [-1, 1].
+    Input to model is waveform of shape (bs, T), and output is logits of shape (bs, len(model.classes))
+    `model.classes` provides the class mapping for each logit. 
+
+    Args:
+        pretrained (bool): load pretrained weights into the model
+        progress (bool): show progress bar when downloading model
+        device (str): device to load model onto ('cuda' or 'cpu' are common choices)
+        type (str): whether to load the "best" checkpoint (lowest validation loss, default), or "last" checkpoint (35k iters).
+    """
+    model = ResNextWrapper()
+
+    if pretrained:
+        ckpt = torch.hub.load_state_dict_from_url("https://github.com/RF5/simple-speech-commands/releases/download/v0.7/resnext-export.pt", 
+                                            progress=progress, map_location=device)
+        model.model.load_state_dict(ckpt)
+    model = model.to(device).eval()
+    model.classes = RESNEXT_CLASSES
+    return model
+        
